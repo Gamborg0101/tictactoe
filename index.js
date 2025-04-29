f4d28b32fdd5e68b65c90a65c9fd98d819ea4ff3
Casper Gamborg <Gamborgc@gmail.com>
2025-04-29 12:55:50 +0200

    Game logic is mostly done

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ const game = () => {
 
   const gameboardFactory = () => {
     const gameBoard = [
-      [0, 0, 0],
-      [0, 0, 0],
-      [0, 0, 0],
+      [0, 0, 1],
+      [0, 1, 0],
+      [1, 0, 0],
     ];
     return { gameBoard };
   };
@@ -34,25 +34,60 @@ const game = () => {
       user2.userTurn = false;
     }
   };
+
   /* Placering af symbol */
   const symbolPlacementLogic = (x, y) => {
     if (user1.userTurn || user2.userTurn) {
       gameBoard[x][y] = user1.playerSymbol;
-      console.log(gameBoard);
-      playerTurnChange();
     }
   };
-  console.log(gameBoard);
 
-  const gameRound = () => {
-    let column = prompt("Give me an column!");
-    let row = prompt("Give me a row!");
-    symbolPlacementLogic(column, row);
-  };
+  /* Tjekker om spiller har vundet */
+  const checkWinner = () => {
+    /* COLUMN CHECK */
 
-  console.log(gameRound());
+    function columnCheck() {
+      gameBoard.map((value) => {
+        if (value[0] == 1 && value[1] == 1 && value[2] == 1) {
+          console.log("Winner with columns");
+        }
+      });
+    }
 
-  const winGameLogic = () => {
+    /* ROW CHECK */
+    function rowCheck() {
+      for (let n = 0; n < gameBoard.length; n++) {
+        let cell1 = gameBoard[0][n];
+        let cell2 = gameBoard[1][n];
+        let cell3 = gameBoard[2][n];
+        if (cell1 == 1 && cell1 == cell2 && cell2 == cell3 && cell3 == cell1) {
+          console.log("Winner with rows");
+        }
+      }
+    }
+
+    /* DIAGONAL CHECK left top - right bottom */
+
+    function diagonalLeftRightCheck() {
+      let cell1 = gameBoard[0][0];
+      let cell2 = gameBoard[1][1];
+      let cell3 = gameBoard[2][2];
+      if (cell1 == 1 && cell1 == cell2 && cell2 == cell3 && cell3 == cell1)
+        console.log("Winner with diagonal (left top - right bottom)");
+    }
+
+    /* DIAGONAL CHECK right top - left bottom */
+    function diagonalRightLeftCheck() {
+      let cell1 = gameBoard[0][2];
+      let cell2 = gameBoard[1][1];
+      let cell3 = gameBoard[2][0];
+      if (cell1 == 1 && cell1 == cell2 && cell2 == cell3 && cell3 == cell1)
+        console.log("Winner with diagonal (right top - left bottom)");
+    }
+    diagonalLeftRightCheck();
+    diagonalRightLeftCheck();
+    rowCheck();
+    columnCheck();
     /* To do som næste: 
     Hvergang der er lavet en ny placering af symbol, så skal arrayet returneres, 
     og så skal det tjekkes med nedstående muligheder:
@@ -63,6 +98,7 @@ const game = () => {
     Also: 
     Forsøg at lave en algortime, som udfylder en helt column eller række - 
     så kan jeg bruge den til at tjekke, om en person har udfyldt en række, i stedet for, at tilfældende skal stå på denne måde.
+    Når jeg har arrayet, så kan jeg iterer igennem det. <-- Løsningen
 
 
 Hvis den har en row? F.eks. row[0] == [x,x,x] || row[1] == [x,x,x] row[2] == [x,x,x]
@@ -101,12 +137,24 @@ Specielle tilfælde:
 [0,X,0]
 [X,0,0]
 
-[X,0,0]
-[0,X,0]
-[0,0,X]
-
     */
   };
+
+  checkWinner();
+
+  /* Logik for hver runde */
+  const playTurn = () => {
+    // let column = parseInt(prompt("Give me an column!"));
+    // let row = parseInt(prompt("Give me a row!"));
+    if (row <= 3 || column <= 3) {
+      console.log("Invaild input");
+    }
+    symbolPlacementLogic(column, row);
+    console.log(gameBoard);
+
+    playerTurnChange();
+  };
+  console.log(playTurn());
 
   const gameEnd = () => {};
 };
