966c4de6d249561bf97bf2648c3aad3a41569656
Casper Gamborg <Gamborgc@gmail.com>
2025-04-30 13:58:31 +0200

    Major cleanup and renaming functions

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const game = () => {
   let user1 = factoryPlayer("Casper", "x");
   let user2 = factoryPlayer("Jens", "o");
 
+  const gameboard = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+
   const starttingPlayer = () => {
     let selectPlayer = Math.floor(Math.random() * 2);
     if (selectPlayer === 0) {
@@ -14,34 +20,25 @@ const game = () => {
       return (user2.userTurn = true);
     }
   };
-  starttingPlayer();
-  //console.log(user1.userTurn);
 
-  const gameboard = () => {
-    return [
-      [0, 0, 0],
-      [0, 1, 0],
-      [1, 0, 0],
-    ];
-  };
-
-  const gameBoard = gameboard();
+  const currentPlayer = () => (user1.userTurn ? user1 : user2);
 
   /* userTurn ændres ved hvert kald. */
-  const playerTurnChange = () => {
-    if (user1.userTurn) {
-      user1.userTurn = false;
-      user2.userTurn = true;
-    } else {
-      user1.userTurn = true;
-      user2.userTurn = false;
-    }
+  const switchTurns = () => {
+    user1.userTurn = !user1.userTurn;
+    user2.userTurn = !user2.userTurn;
   };
 
   /* Placering af symbol */
-  const symbolPlacementLogic = (x, y) => {
-    if (user1.userTurn || user2.userTurn) {
-      gameBoard[x][y] = user1.playerSymbol;
+  const placeSymbol = (x, y) => {
+    const player = currentPlayer();
+
+    if (gameboard[x][y] === "") {
+      gameboard[x][y] = player.symbol;
+      return true;
+    } else {
+      console.log("Field already taken");
+      return false;
     }
   };
 
@@ -51,7 +48,7 @@ const game = () => {
         checkSignleColumn(n);
       }
       function checkSignleColumn(n) {
-        if (gameBoard.map((column) => column[n]).every((index) => index == 1)) {
+        if (gameboard.map((column) => column[n]).every((index) => index == 1)) {
           return console.log("Winner with columns", n);
         }
       }
@@ -62,7 +59,7 @@ const game = () => {
         checkSingleRow(n);
       }
       function checkSingleRow(n) {
-        if (gameBoard[n].map((column) => column).every((index) => index == 1)) {
+        if (gameboard[n].map((column) => column).every((index) => index == 1)) {
           return console.log("Winner with rows", n);
         }
       }
@@ -71,7 +68,7 @@ const game = () => {
     function checkDiagonalLeftRight() {
       const diagonalCoordinatsLeft = [];
       for (let n = 0; n < 3; n++) {
-        diagonalCoordinatsLeft.push(gameBoard[n][n]);
+        diagonalCoordinatsLeft.push(gameboard[n][n]);
       }
       if (diagonalCoordinatsLeft.every((index) => index == 1)) {
         console.log("Winner with diagonalCoordinatsLeft");
@@ -81,42 +78,31 @@ const game = () => {
     function checkDiagonalRightLeft() {
       const diagonalCoordinatsRight = [];
       for (let n = 0; n < 3; n++) {
-        diagonalCoordinatsRight.push(gameBoard[n][2 - n]);
+        diagonalCoordinatsRight.push(gameboard[n][2 - n]);
       }
       if (diagonalCoordinatsRight.every((index) => index == 1)) {
         console.log("Winner with checkDiagonalRightLeft");
       }
     }
-    checkColumn();
-    checkRow();
-    checkDiagonalLeftRight();
-    checkDiagonalRightLeft();
   };
 
-  if (user1.userTurn) {
-  } else {
-  }
-
-  /* Logik for hver runde */
-  const playTurn = () => {
-    // let column = parseInt(prompt("Give me an column!"));
-    //let row = parseInt(prompt("Give me a row!"));
-    let row = 0;
-    let column = 0;
-    if (row <= 3 || column <= 3) {
-      console.log("Invaild input");
-    }
-    symbolPlacementLogic(column, row);
-
-    playerTurnChange();
+  const playTurn = (x, y) => {
+    if (!placeSymbol(x, y)) return;
 
     if (checkWinner()) {
-      console.log("stop game");
+      console.log("Game over!");
+      return;
     }
+    switchTurns();
+    console.log("Next turn:", currentPlayer().name);
   };
-  console.log(playTurn());
 
-  const gameEnd = () => {};
+  starttingPlayer();
+  console.log("Starting player: ", currentPlayer().name);
+  playTurn(1, 1);
+  console.log(gameboard);
+  playTurn(0, 1);
+  console.log(gameboard);
 };
 
 game();
